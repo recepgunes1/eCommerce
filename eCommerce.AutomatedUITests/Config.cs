980b3802cbe376dbe7fc4e71b912d578c2d559d5
Recep Gunes <recepgunes2@outlook.com>
2023-07-25 08:46:28 +0300

    Update Config.cs

--- eCommerce.AutomatedUITests/Config.cs
+++ eCommerce.AutomatedUITests/Config.cs
@@ -4,6 +4,7 @@ namespace eCommerce.AutomatedUITests
 {
     public static class Config
     {
+        //test
         public readonly static string AppUrl = "https://localhost:7109";
         public readonly static string DriverDirectory = "http://172.22.80.1:4444/wd/hub";
         public static ChromeDriver ChromeDriver
